d2b07ea524030eb4dcd82b5191a2b196bd346008
Leeeon233 <leeeon233@gmail.com>
2026-08-22 11:08:08 +0800

    chore: refresh Nix dependency hash

--- flake.nix
+++ flake.nix
@@ -162,7 +162,7 @@
               inherit (finalAttrs) pname version src pnpmWorkspaces;
               inherit pnpm;
               fetcherVersion = 3;
-              hash = "sha256-P450+LKDYkRyk7OZ2mSOX0/RwtbivwR5ZksN8FM6+TU=";
+              hash = "sha256-x+hSxQ9NzYnHy9uaw/hHKBGv5SeBdmHuqSKHGpGHagw=";
             };
 
             nativeBuildInputs = [
